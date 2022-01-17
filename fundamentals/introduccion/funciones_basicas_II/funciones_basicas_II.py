cada0f5c7ad9c56c94d27c2440f40c7e9a05ef26
nataly garcia <naty_garcioli@hotmail.com>
2022-01-17 16:18:43 -0500

    listo

--- fundamentals/introduccion/funciones_basicas_II/funciones_basicas_II.py
+++ fundamentals/introduccion/funciones_basicas_II/funciones_basicas_II.py
@@ -1,8 +1,17 @@
-def lista_regresiva(numero):
-  array = []
-  for i in range(numero, 0, -1):
-    print(i)
-    array.append(i)
-  return array
+# def lista_regresiva(numero):
+#   array = []
+#   for i in range(numero, 0, -1):
+#     print(i)
+#     array.append(i)
+#   return array
 
-print(lista_regresiva(5))
+# print(lista_regresiva(5))
+
+
+# [2,3]
+def recibe_dos_numeros(lista):
+  print(lista[0])
+  return lista[1]
+
+array = [1,2]
+print(recibe_dos_numeros(array))
