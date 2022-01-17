bb0e86017b1c2564eda3ae580369f8054c734577
nataly garcia <naty_garcioli@hotmail.com>
2022-01-17 12:22:00 -0500

    bonu

--- fundamentals/introduccion/hola_mundo/hello_world.py
+++ fundamentals/introduccion/hola_mundo/hello_world.py
@@ -7,7 +7,7 @@ print( "Hola " + str(name)  )	# con un +
 # 3. imprimir "Hola 42!" con el número en una variable
 name = 42
 print( f"Hola, {name}!" )	# con una coma
-# print( "hola" + name  )	# con una +	-- este debería arrojar un error!
+print( "hola " + str(name)  )	# con una +	-- este debería arrojar un error!
 # 4. imprimir "Amo comer sushi y pizza" con las comidas en variables
 fave_food1 = "sushi"
 fave_food2 = "pizza"
