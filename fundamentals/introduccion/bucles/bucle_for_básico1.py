aa4be5b090f951812e7d79ddd3c053c54acf947e
nataly garcia <naty_garcioli@hotmail.com>
2022-01-17 13:27:21 -0500

    funciona a medias bucle 3

--- fundamentals/introduccion/bucles/bucle_for_básico1.py
+++ fundamentals/introduccion/bucles/bucle_for_básico1.py
@@ -1,14 +1,17 @@
+#ejercicio 1
 for x in range(0,151):
   print(x)
-  
+
+#ejercicio 2
 for x in range(5, 1001, 5):
   print(x)
 
-
+#ejercicio 3
 count = 1
-while (count % 5 == 0):
-  print("Coding")
+while (count <= 101):
+  print(count)
+  if count % 5 == 0:
+    print("Coding")
+  elif count % 10 == 0:
+    print("Coding Dojo")
   count += 1
-else:
-  print("Coding Dojo")
-  
