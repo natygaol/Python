f0c1ba1f99a960f40b47f5bf824adebad238d61b
nataly garcia <naty_garcioli@hotmail.com>
2022-01-17 12:59:53 -0500

    bucles hasta el 3

---
+++ fundamentals/introduccion/bucles/bucle_for_básico1.py
@@ -0,0 +1,14 @@
+for x in range(0,151):
+  print(x)
+  
+for x in range(5, 1001, 5):
+  print(x)
+
+
+count = 1
+while (count % 5 == 0):
+  print("Coding")
+  count += 1
+else:
+  print("Coding Dojo")
+  
