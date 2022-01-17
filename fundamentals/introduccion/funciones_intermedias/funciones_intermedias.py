69cae673de943d902184b8c999030d9b153a4f9f
nataly garcia <naty_garcioli@hotmail.com>
2022-01-17 18:18:25 -0500

    ejercicio 3

--- fundamentals/introduccion/funciones_intermedias/funciones_intermedias.py
+++ fundamentals/introduccion/funciones_intermedias/funciones_intermedias.py
@@ -31,4 +31,39 @@ print(z)
 
 """PARTE 2"""
 
-#
+"""Crea una función iterateDictionary(some_list)para que, dada una lista de diccionarios, 
+la función recorra cada diccionario de la lista e 
+imprima cada llave y el valor asociado. """
+
+# def iterateDictionary(some_list):
+#   for i in some_list[0].items:
+#     print(i)
+#     #sfirst_name - Michael, last_name - Jordan
+    
+
+estudiantes1 = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+# iterateDictionary(estudiantes1) 
+
+
+"""PARTE 3"""
+
+""" dada una lista de diccionarios y un nombre de clave, 
+la función imprima el valor almacenado en esa clave para cada diccionario. 
+Por ejemplo, iterateDictionary2('name', estudiantes) 
+Michael
+John
+Mark
+KB"""
+def iterateDictionary2(key_name, some_list):
+  for dictionary in some_list:
+    # {'first_name': 'Michael', 'last_name': 'Jordan'} me da key/value
+    for key_name in dictionary:
+      print(dictionary[key_name])
+      
+
+iterateDictionary2("first_name", estudiantes1)
