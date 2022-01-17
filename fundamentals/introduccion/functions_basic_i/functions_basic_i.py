3101d8cc6ec7f883b1f99119cc8869d336eb3b1b
nataly garcia <naty_garcioli@hotmail.com>
2022-01-17 15:47:54 -0500

    comments

--- fundamentals/introduccion/functions_basic_i/functions_basic_i.py
+++ fundamentals/introduccion/functions_basic_i/functions_basic_i.py
@@ -1,123 +1,128 @@
-# #1
-# def number_of_food_groups():
-#     return 5
-# print(number_of_food_groups())
+#1
+def number_of_food_groups():
+    return 5
+print(number_of_food_groups())
 
+# 5
 
-# #2
-# def number_of_military_branches():
-#     return 5
-# print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+#2
+def number_of_military_branches():
+    return 5
+print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+# error porque no hay una funcion de ese nombre
 
+#3
+def number_of_books_on_hold():
+    return 5
+    return 10
+print(number_of_books_on_hold())
+# sale 10
 
-# #3
-# def number_of_books_on_hold():
-#     return 5
-#     return 10
-# print(number_of_books_on_hold())
+#4
+def number_of_fingers():
+    return 5
+    print(10)
+print(number_of_fingers())
+#despues del return no lee nada más
+#5
 
+#5
+def number_of_great_lakes():
+    print(5)
+x = number_of_great_lakes()
+print(x)
+#preguntar porque sale none y porque 5 no se guarda ahí
+# 5
 
-# #4
-# def number_of_fingers():
-#     return 5
-#     print(10)
-# print(number_of_fingers())
-# #despues del return no lee nada más
-
-# #5
-# def number_of_great_lakes():
-#     print(5)
-# x = number_of_great_lakes()
-# print(x)
-# #preguntar porque sale none y porque 5 no se guarda ahí
-
-#6
-# def add(b,c):
-#     print(b+c)
-# print(add(1,2) + add(2,3))
-# no se puede sumar funciones?
+# 6
+def add(b,c):
+    print(b+c)
+print(add(1,2) + add(2,3))
+#no se puede sumar funciones?
 
 #7
-# def concatenate(b,c):
-#     return str(b)+str(c)
-# print(concatenate(2,5))
+def concatenate(b,c):
+    return str(b)+str(c)
+print(concatenate(2,5))
+# 25
 
+#8
+def number_of_oceans_or_fingers_or_continents():
+    b = 100
+    print(b)
+    if b < 10:
+        return 5
+    else:
+        return 10
+    return 7
+print(number_of_oceans_or_fingers_or_continents())
+# 10
 
-# #8
-# def number_of_oceans_or_fingers_or_continents():
-#     b = 100
-#     print(b)
-#     if b < 10:
-#         return 5
-#     else:
-#         return 10
-#     return 7
-# print(number_of_oceans_or_fingers_or_continents())
+9
+def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
+    if b<c:
+        return 7
+    else:
+        return 14
+    return 3
+print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
+print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+# 7
+# 14
+# 21 porque si se suma
 
+#10
+def addition(b,c):
+    return b+c
+    return 10
+print(addition(3,5))
+#8
 
-#9
-# def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
-#     if b<c:
-#         return 7
-#     else:
-#         return 14
-#     return 3
-# print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
-# print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
-# print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
-
-
-# #10
-# def addition(b,c):
-#     return b+c
-#     return 10
-# print(addition(3,5))
-
-
-# #11
-# b = 500
-# print(b)
-# def foobar():
-#     b = 300
-#     print(b)
-# print(b)
-# foobar()
-# print(b)
-
+#11
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+print(b)
+foobar()
+print(b)
+# 500, 500, 300, 500
 
 #12
-# b = 500
-# print(b)
-# def foobar():
-#     b = 300
-#     print(b)
-#     return b
-# print(b)
-# foobar()
-# print(b)
-
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+    return b
+print(b)
+foobar()
+print(b)
+#500, 500, 300, 500
 
 #13
-# b = 500
-# print(b)
-# def foobar():
-#     b = 300
-#     print(b)
-#     return b
-# print(b)
-# b=foobar()
-# print(b)
-
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+    return b
+print(b)
+b=foobar()
+print(b)
+# 500, 500, 300, 300
 
 #14
-# def foo():
-#     print(1)
-#     bar()
-#     print(2)
-# def bar():
-#     print(3)
-# foo()
-
+def foo():
+    print(1)
+    bar()
+    print(2)
+def bar():
+    print(3)
+foo()
+# 1, 3, 2
 
 #15
 def foo():
@@ -130,3 +135,5 @@ def bar():
     return 5
 y = foo()
 print(y)
+
+# preguntar porque no estoy segura
