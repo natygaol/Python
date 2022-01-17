6fd2af146c13ca8bc29bc41d73074336f88213de
nataly garcia <naty_garcioli@hotmail.com>
2022-01-17 12:13:37 -0500

    plantilla lista para llenar

---
+++ fundamentals/introduccion/hola_mundo/hello_world.py
@@ -0,0 +1,15 @@
+# 1. TAREA: imprime "Hola, mundo"
+print(  )
+# 2. imprime "Hola, Noelle" con el nombre en una variable
+name = "Noelle"
+print(  )	# con una coma
+print(  )	# con un +
+# 3. imprimir "Hola 42!" con el número en una variable
+name = 42
+print(  )	# con una coma
+print(  )	# con una +	-- este debería arrojar un error!
+# 4. imprimir "Amo comer sushi y pizza" con las comidas en variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print(  ) # con .format()
+print(  ) # con una cadena f
