891b955112813b851d4fc927ef9a7475e6c64484
nataly garcia <naty_garcioli@hotmail.com>
2022-01-17 18:33:26 -0500

    segundo algoritmo ok

---
+++ fundamentals/introduccion/funciones_intermedias/aparte.py
@@ -0,0 +1,30 @@
+x = [ [5,2,3], [10,8,9] ]
+
+estudiantes = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+directorio_deportes = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'fútbol' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+
+"""PARTE 1"""
+# Cambia el valor 10 en x a 15. Una vez que hayas terminado, x ahora debería ser [ [5,2,3], [15,8,9] ].
+x[1][0] = 15
+print(x)
+
+# Cambia el "apellido” del primer alumno de 'Jordan' a 'Bryant'.
+estudiantes[0]["last_name"] = "Bryant"
+print(estudiantes)
+
+# En el directorio_deportes, cambia "Messi" por "Andrés".
+directorio_deportes["fútbol"][0] = "Andrés"
+print(directorio_deportes)
+
+# Cambia el valor 20 en z a 30.
+z[0]["y"] = 30
+print(z)
+
--- fundamentals/introduccion/funciones_intermedias/funciones_intermedias.py
+++ fundamentals/introduccion/funciones_intermedias/funciones_intermedias.py
@@ -1,33 +1,3 @@
-x = [ [5,2,3], [10,8,9] ]
-
-estudiantes = [
-     {'first_name':  'Michael', 'last_name' : 'Jordan'},
-     {'first_name' : 'John', 'last_name' : 'Rosales'}
-]
-directorio_deportes = {
-    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
-    'fútbol' : ['Messi', 'Ronaldo', 'Rooney']
-}
-z = [ {'x': 10, 'y': 20} ]
-
-
-"""PARTE 1"""
-# Cambia el valor 10 en x a 15. Una vez que hayas terminado, x ahora debería ser [ [5,2,3], [15,8,9] ].
-x[1][0] = 15
-print(x)
-
-# Cambia el "apellido” del primer alumno de 'Jordan' a 'Bryant'.
-estudiantes[0]["last_name"] = "Bryant"
-print(estudiantes)
-
-# En el directorio_deportes, cambia "Messi" por "Andrés".
-directorio_deportes["fútbol"][0] = "Andrés"
-print(directorio_deportes)
-
-# Cambia el valor 20 en z a 30.
-z[0]["y"] = 30
-print(z)
-
 
 """PARTE 2"""
 
@@ -35,11 +5,21 @@ print(z)
 la función recorra cada diccionario de la lista e 
 imprima cada llave y el valor asociado. """
 
-# def iterateDictionary(some_list):
-#   for i in some_list[0].items:
-#     print(i)
-#     #sfirst_name - Michael, last_name - Jordan
-    
+def iterateDictionary(some_list):
+  for dictionary in some_list:
+    names = ""
+    for element in dictionary:
+      names += element + ' - ' +  dictionary[element]
+      if(names == "last_name"):
+        names += ""
+      else:
+        names += ","
+    print(names)
+      # if(element == "last_name"):
+      #   ""
+      
+    #first_name - Michael, last_name - Jordan
+
 
 estudiantes1 = [
          {'first_name':  'Michael', 'last_name' : 'Jordan'},
@@ -47,7 +27,8 @@ estudiantes1 = [
          {'first_name' : 'Mark', 'last_name' : 'Guillen'},
          {'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]
-# iterateDictionary(estudiantes1) 
+
+iterateDictionary(estudiantes1)
 
 
 """PARTE 3"""
@@ -59,11 +40,11 @@ Michael
 John
 Mark
 KB"""
-def iterateDictionary2(key_name, some_list):
-  for dictionary in some_list:
-    # {'first_name': 'Michael', 'last_name': 'Jordan'} me da key/value
-    for key_name in dictionary:
-      print(dictionary[key_name])
-      
+# def iterateDictionary2(key_name, some_list):
+#   for dictionary in some_list:
+#     # {'first_name': 'Michael', 'last_name': 'Jordan'} me da key/value
+#     for key_name in dictionary:
+#       print(dictionary[key_name])
 
-iterateDictionary2("first_name", estudiantes1)
+
+# iterateDictionary2("first_name", estudiantes1)
