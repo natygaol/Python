b9290e7ded9d939d93e651091540b22ae15021eb
nataly garcia <naty_garcioli@hotmail.com>
2022-01-17 12:09:36 -0500

    comentarios listos

--- fundamentals/introduccion/reconocer_python/file.py
+++ fundamentals/introduccion/reconocer_python/file.py
@@ -6,64 +6,65 @@ pizza_toppings = ['Pepperoni', 'Sausage', 'Jalepenos', 'Cheese', 'Olives'] #vari
 person = {'name': 'John', 'location': 'Salt Lake', 'age': 37, 'is_balding': False} #variable declaration, inicializo diccionario
 fruit = ('blueberry', 'strawberry', 'banana') #variable declaration, inicializo tupla
 print(type(fruit)) #type check
-print(pizza_toppings[1]) 
-pizza_toppings.append('Mushrooms')
-print(person['name'])
-person['name'] = 'George'
-person['eye_color'] = 'blue'
-print(fruit[2])
+print(pizza_toppings[1]) #Data types / Composite / List / access value
+pizza_toppings.append('Mushrooms') #Data types / Composite / List / add value
+print(person['name']) #Data types / Composite / Diccionario / access value
+person['name'] = 'George' #Data types / Composite / Diccionario / change value
+person['eye_color'] = 'blue' #Data types / Composite / Diccionario / add value
+print(fruit[2]) # Data types / Composite / Tuples / access value
 
-if num1 > 45:
+if num1 > 45:             # condicional / if
     print("It's greater")
-else:
+else:                     # condicional / else
     print("It's lower")
 
-if len(string) < 5:
+if len(string) < 5:             # condicional / if
     print("It's a short word!")
-elif len(string) > 15:
+elif len(string) > 15:          # condicional / else if
     print("It's a long word!")
-else:
+else:                           # condicional / else
     print("Just right!")
 
-for x in range(5):
+for x in range(5): #for loop del 0 al 4
     print(x)
-for x in range(2,5):
+for x in range(2,5): # for loop del 2 al 4
     print(x)
-for x in range(2,10,3):
+for x in range(2,10,3): # for loop del 2 al 9 pero de 3 en 3 se corre
     print(x)
-x = 0
-while(x < 5):
+
+x = 0 #variable declaration
+while(x < 5): #while loop start
     print(x)
-    x += 1
+    x += 1  #while loop / increment
 
-pizza_toppings.pop()
-pizza_toppings.pop(1)
+pizza_toppings.pop() #Data types / Composite / List / delete value
+pizza_toppings.pop(1) #Data types / Composite / List / delete value del indice 1
 
-print(person)
-person.pop('eye_color')
-print(person)
+print(person) # imprimir valor de variable person
+person.pop('eye_color') #Data types / Composite / Diccionario / delete key -> eye_color
+print(person) # imprimir el diccionario luego de modificaciones
 
-for topping in pizza_toppings:
-    if topping == 'Pepperoni':
-        continue
-    print('After 1st if statement')
-    if topping == 'Olives':
-        break
+for topping in pizza_toppings: #for loop
+    if topping == 'Pepperoni': # condicion
+        continue #continue
+    print('After 1st if statement') 
+    if topping == 'Olives': #condicion
+        break               #break
 
-def print_hello_ten_times():
-    for num in range(10):
-        print('Hello')
+def print_hello_ten_times(): #funcion
+    for num in range(10): #argumento
+        print('Hello') 
 
 print_hello_ten_times()
 
-def print_hello_x_times(x):
-    for num in range(x):
-        print('Hello')
+def print_hello_x_times(x): #funcion / parametro
+    for num in range(x): #argumento
+        print('Hello') 
 
-print_hello_x_times(4)
+print_hello_x_times(4) # imprime 4 veces
 
-def print_hello_x_or_ten_times(x = 10):
-    for num in range(x):
+def print_hello_x_or_ten_times(x = 10): #funcion / parametro
+    for num in range(x): #argumento
         print('Hello')
 
 print_hello_x_or_ten_times()
@@ -74,11 +75,11 @@ print_hello_x_or_ten_times(4)
 Bonus section
 """
 
-# print(num3)
+# print(num3) -> NameError: name 'num3' is not defined
 # num3 = 72
-# fruit[0] = 'cranberry'
-# print(person['favorite_team'])
-# print(pizza_toppings[7])
-#   print(boolean)
-# fruit.append('raspberry')
-# fruit.pop(1)
+# fruit[0] = 'cranberry' -> TypeError: 'tuple' object does not support item assignment
+# print(person['favorite_team']) 
+# print(pizza_toppings[7]) -> KeyError: 'favorite_team'
+#   print(boolean) -> IndentationError: unexpected indent
+# fruit.append('raspberry') -> AttributeError: 'tuple' object has no attribute 'append'
+# fruit.pop(1) -> AttributeError: 'tuple' object has no attribute 'pop'
