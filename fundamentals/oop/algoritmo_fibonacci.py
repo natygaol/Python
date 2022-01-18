b60e5fb8464ebf8434bf3f7296f2a3d3aa59d417
nataly garcia <naty_garcioli@hotmail.com>
2022-01-18 09:46:11 -0500

    fibonacci ok

--- fundamentals/oop/algoritmo_fibonacci.py
+++ fundamentals/oop/algoritmo_fibonacci.py
@@ -1,7 +1,19 @@
 # Escribe una función llamada fibonacci. Esta función recibe como parámetro un 
 # número entero positivo. La función se encarga de imprimir la serie de fibonacci.
 # El número recibido como parámetro es la cantidad de números a imprimir.
-# 0 1 1 2 3 5 8 13 21 34 55 89
+# 30 => [ 1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 ]
+
 
 def fibonacci(num):
-  
+  x1 = 0
+  x2 = 1
+  for i in range(0, num):
+    if(i % 2 == 0):
+      print(x1)
+      x1 += x2
+    else:
+      print(x2)
+      x2 += x1
+
+
+fibonacci(10)
