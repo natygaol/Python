abaa526ee74aa35457782549e9eef90565a779a5
nataly garcia <naty_garcioli@hotmail.com>
2022-01-18 11:29:05 -0500

    partial listo

---
+++ fundamentals/oop/practica_usuario_optional/Usuario.py
@@ -0,0 +1,18 @@
+class Usuario:
+  #constructor
+  def __init__(self , name, email_address):
+    self.name = name
+    self.email = email_address
+    self.balance_cuenta = 0
+  
+  def hacer_deposito(self, amount):
+    # le sumo el dinero que solicita
+    self.hacer_deposito += amount
+
+  def hacer_retiro(self, amount):
+    # le resto el dinero que solicita
+    self.balance_cuenta -= amount
+
+  def mostrar_balance_usuario(self):
+    # le muestro cuanto dinero tiene actualmente llamando al balance
+    return self.balance_cuenta
